2841c92bbc6aaced7c88e1a743929efb137d57f6
Melvin Adkins <111800396+mel24ncf@users.noreply.github.com>
2025-02-10 23:31:30 -0500

    Update chd_explore.py

--- Supervised Learning/Framingham Heart Study/chd_explore.py
+++ Supervised Learning/Framingham Heart Study/chd_explore.py
@@ -1,15 +1,16 @@
+# load libraries
 import pandas as pd
-from pandas_profiling import ProfileReport
+from ydata_profiling import ProfileReport
 
+# name report
 report_filename = "data_report.html"
 
 # read framingham.csv into a pandas dataframe
-df = pd.read_csv("Data/Framingham/framingham.csv")
+df = pd.read_csv("framingham.csv")
 
 # use pandas_profiling.ProfileReport to create a report
 profile = ProfileReport(df, title="Framingham Heart Study Data Report")
 
 # save report to data_report.html
 profile.to_file(output_file=report_filename)
-
 print(f"Wrote report to {report_filename}")
